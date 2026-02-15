6f73c4c67372b3a2c6ae4dd831a492c3f1c75895
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2026-02-15 01:19:11 -0800

    *SASS

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,23 @@
 import "./style.scss"
-
+import LOGO from "../../assets/Images/SpreadItLogo.png"
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="container">
+      <div className="navbar">
+        <div className="logoDiv">
+          <img src={LOGO} alt="logo" className="logo" />
+        </div>
+        <div>
+          <ul className="tabs">
+            <li className="tab">How It Works</li>
+            <li className="tab">Download</li>
+            <li className="tab">Upgrade</li>
+            <li className="tab">Feedback</li>
+            <li className="tab">Login/Register</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 };
 
